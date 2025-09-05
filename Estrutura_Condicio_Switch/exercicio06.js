199bf2007117d3a7c5891703652dc16f3d8d51eb
Luis Fernando <luisfernandonunesdacosta@gmail.com>
2025-09-05 15:18:53 -0300

    Atualização
    Adicionados novos "cases" do exercício 06.

--- Estrutura_Condicio_Switch/exercicio06.js
+++ Estrutura_Condicio_Switch/exercicio06.js
@@ -1,15 +1,37 @@
 const nota1 = 8;
 const nota2 = 7;
 const nota3 = 9;
-const opcao = 1;
+const opcao = 4;
 let resultadoMedia;
 let str = " ";
+
+console.log("Escolha o tipo de Média desejada: \n 1-aritmética,\n2-ponderada, \n3-harmônica, \n 4-quadrática");
 
 switch(opcao){
     case 1:
     resultadoMedia = (nota1 + nota2 + nota3) / 3;
 
-
     str = "Opção:"+opcao+" escolhida! A média é: "+ resultadoMedia;
+break;
+    case 2:
+
+    resultadoMedia = (nota1 * 3 + nota2 * 3 + nota3 * 4) / (3 + 3 + 4);
+
+    str = "Opção:"+opcao+" escolhida! A média Ponderada é: "+ resultadoMedia;
+break;
+ 
+    case 3: 
+    resultadoMedia = 3/(1/nota1 + 1/nota2 + 1/nota3);
+
+    str = "Opção:"+opcao+" escolhida! A média Harmônica é: "+ resultadoMedia;
+break;
+    case 4:    
+    resultadoMedia = Math.sqrt((nota1 * nota1 + nota2 * nota2 + nota3 * nota3) / 3);
+
+    str = "Opção:"+opcao+" escolhida! A média Quadrática é: "+ resultadoMedia;
+break;
+
 }
+
+
 console.log(str);
