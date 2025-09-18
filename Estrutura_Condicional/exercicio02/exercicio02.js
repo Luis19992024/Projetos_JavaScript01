15a3b2ff912357024f421083a98b25fa75065b5b
Luis Fernando <luisfernandonunesdacosta@gmail.com>
2025-09-18 14:47:38 -0300

    Novas Atividades e Atualizações de Código
    Foram criadas novas atividades e novos códigos incorporados aos existentes.

---
+++ Estrutura_Condicional/exercicio02/exercicio02.js
@@ -0,0 +1,12 @@
+let salario = 1200;
+
+if(salario<=1412){
+    
+let aumentoSalarial = salario * 1.5;
+
+console.log(" Salário reajustado em 50%, R$: " + aumentoSalarial);
+} else if(salario > 1412){
+    let aumentoSalarial = salario * 1.4;
+
+    console.log(" Salário reajustado em 40%, R$: " + aumentoSalarial);
+}
